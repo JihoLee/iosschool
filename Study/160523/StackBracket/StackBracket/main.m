d314e61730c67cf913aaa10c70ce9945ed6a7a0b
JihoLee <najanda89@gmail.com>
2016-05-23 11:33:53 +0900

    바른괄호 체크 프로그램

--- Study/160523/StackBracket/StackBracket/main.m
+++ Study/160523/StackBracket/StackBracket/main.m
@@ -7,11 +7,52 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <string.h>
+#include <stdio.h>
+
+char str[50];
+int n;
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        int t;
+        scanf("%d", &t);
+        
+        for (int k = 0; k < t; k++) {
+            scanf("%s", str);
+            int n = strlen(str);
+            
+            int cnt = 0;
+            int negative = 0;
+
+            for (int i = 0; i < n; i++) {
+            
+                char c = str[i];
+                
+                if( c== '(') {
+                    
+                    cnt += 1;
+                }
+                
+                else {
+                    cnt -= 1;
+                }
+                
+                if(cnt < 0 ) {
+                    negative = 1;
+                }
+                
+            }
+            
+            if(negative == 0 && cnt == 0) {
+                printf("YES\n");
+            }
+            else {
+                printf("NO\n");
+            }
+        }
+        
     }
     return 0;
 }
