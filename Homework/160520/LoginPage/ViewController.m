7d7d9c6f12ecbf46e6c4cfd68aac6a54f331cdfd
JihoLee <najanda89@gmail.com>
2016-05-27 09:50:50 +0900

    로그인 페이지 method 수정

--- Homework/160520/LoginPage/ViewController.m
+++ Homework/160520/LoginPage/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+
+@property (nonatomic, weak) IBOutlet UITextField *userIdTextField;
+@property (nonatomic, weak) IBOutlet UITextField *passwordTextField;
+
+
 
 @end
 
@@ -16,12 +21,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.userIdTextField.tag = 1;
+    self.passwordTextField.tag = 2;
+    
+    self.userIdTextField.delegate = self;
+    self.passwordTextField.delegate = self;
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)loginEvent:(id)sender {
+    
+    NSString *userId = self.userIdTextField.text;
+    NSString *password = self.passwordTextField.text;
+    
+    NSLog(@"user Id : %@, password : %@", userId, password);
+    
+    [self.userIdTextField resignFirstResponder];
+    [self.passwordTextField resignFirstResponder];
+    
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    
+    return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    
+    if(textField == self.userIdTextField) {
+        [self.userIdTextField becomeFirstResponder];
+    }
+    else if(textField == self.passwordTextField) {
+        
+        [self.passwordTextField becomeFirstResponder];
+    }
+    
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSLog(@"aa");
+    if(textField.tag == 1) {
+        NSLog(@"userid");
+        [self.passwordTextField becomeFirstResponder];
+    } else if(textField.tag == 2) {
+        [self.passwordTextField resignFirstResponder];
+    }
+    
+    return YES;
 }
 
 @end
