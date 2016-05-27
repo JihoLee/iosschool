b040c5ae8ef1fa0bc5805f228b1a3e148c653be5
JihoLee <najanda89@gmail.com>
2016-05-27 19:23:40 +0900

    알고리즘 실습 - 수학

--- Study/160525/MathExample/MathExample/MyClass.m
+++ Study/160525/MathExample/MathExample/MyClass.m
@@ -10,4 +10,39 @@
 
 @implementation MyClass
 
+
+- (void) primesCount {
+    
+    NSInteger limit = 100000;
+    NSMutableArray *prime = [@[] mutableCopy];
+    // Check가 true면 소수(지워지지 않음)이다, false면 소수가 아니다(지워짐)
+    NSMutableArray *check = [NSMutableArray arrayWithCapacity:limit+1];
+    
+    
+    for (NSInteger i = 0; i <= limit; i++) {
+        [check addObject:@(YES)];
+        //            [check addObject:[NSNumber numberWithBool:NO]];
+        
+    }
+    
+    
+    NSLog(@"%zu", [check count]);
+    
+    for (NSInteger i = 2; i <= limit; i++) {
+        if ([check[i] boolValue] == YES) {
+            [prime addObject:@(i)];
+            
+            for (NSInteger j = i*i; j<=limit; j+=i) {
+                check[j] = @(NO);
+                
+            }
+        }
+    }
+    
+    
+    NSLog(@"%zu", [prime count]);
+    
+}
+
+
 @end
--- Study/160525/MathExample/MathExample/main.m
+++ Study/160525/MathExample/MathExample/main.m
@@ -8,10 +8,119 @@
 
 #import <Foundation/Foundation.h>
 
+//int main(int argc, const char * argv[]) {
+//    @autoreleasepool {
+//        NSInteger count = 10;
+//        NSInteger nextNumber = 0;
+//        NSMutableArray *pibonacci = [[NSMutableArray alloc] initWithObjects:@0,@1, nil];
+//        
+//        for (int i = 0; i < count - 1; i++) {
+//            
+//            NSInteger leftNumber = [[pibonacci objectAtIndex:i] integerValue];
+//            NSInteger rightNumber = [[pibonacci objectAtIndex:i+1] integerValue];
+//            
+//            nextNumber = leftNumber + rightNumber;
+//            
+//            [pibonacci addObject:[NSNumber numberWithInteger:nextNumber]];
+//        
+//        }
+//        
+//        
+//            NSLog(@"%@ ", [pibonacci objectAtIndex:count]);
+//        
+//        
+//        
+//        
+//        
+//        
+//    }
+//    return 0;
+//}
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    
+    int pibonacci[10000]={0,1};
+    int n = 0;
+    int nextNumber = 0;
+    
+    scanf("%d", &n);
+    
+    
+    for (int i = 0; i < n ; i++) {
+        
+        int leftNumber = pibonacci[i];
+        int rightNumber = pibonacci[i+1];
+        
+        nextNumber = leftNumber + rightNumber;
+        
+        pibonacci[i+2] = nextNumber;
+//        printf("%d\n", pibonacci[i+2]);
     }
+    
+    
+    printf("%d\n", pibonacci[n]);
+//    int n = 0;
+//    scanf("%d", &n);
+//    int nemo[n];
+//    int count = 1;
+//    for (int i = 0; i < n; i++) {
+//        nemo[i] = 1;
+//    }
+//    int result = 0;
+//    for (int j = 0; j < n; j++) {
+//        
+//        result = nemo[j] + nemo[j+1];
+//        
+//        if(result == 1) {
+//            count--;
+//        }
+//        else if(result == 2) {
+//            count++;
+//        }
+//        
+//        
+//        
+//    }
+//    
+//    for (int k = 0; k < n; k++) {
+//        result = nemo[k] + nemo[k+1];
+//        result = nemo[k+2] + nemo[k+3];
+//        
+//    }
+//    
+//    printf("%d\n", count);
+    
     return 0;
 }
+
+//int main(int argc, const char * argv[]) {
+//    
+//    int n = 0;
+//    int count = 0;
+//    scanf("%d", &n);
+//    
+//    while (n != 1) {
+//        if(n % 3 == 0) {
+//            
+//            n = n / 3;
+//            count += 1;
+//            continue;
+//        }
+//        
+//        if(n % 2 == 0) {
+//            
+//            n = n / 2;
+//            count += 1;
+//            continue;
+//        }
+//        
+//        if(n != 1) {
+//            n -= 1;
+//            continue;
+//        }
+//        
+//        
+//    }
+//    
+//    printf("%d\n", count);
+//
+//}
